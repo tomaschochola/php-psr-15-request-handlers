d51bd4eb2e3f29f54bf62bca87dbb678d5144088
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-30 22:17:19 +0200

    Update: src/RouteRequestHandler.php | Delete: src/AfterPipeline.php, src/AfterPipelineInterface.php, src/BeforePipeline.php, src/BeforePipelineInterface.php, src/PipelineResolver.php, src/PipelineResolverInterface.php

--- src/RouteRequestHandler.php
+++ src/RouteRequestHandler.php
@@ -15,55 +15,121 @@ declare(strict_types=1);
 
 namespace TomasChochola\Psr\Http\RequestHandlers;
 
+use Iterator;
 use NoDiscard;
 use Override;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+
+use function assert;
+use function explode;
+use function is_array;
+use function is_iterable;
+use function is_string;
+use function rawurldecode;
 
 /**
  * @no-named-arguments
  */
 readonly class RouteRequestHandler implements RequestHandlerInterface
 {
-    private readonly AfterPipelineInterface $after;
+    private readonly ContainerInterface $container;
 
-    private readonly BeforePipelineInterface $before;
+    private readonly RouteSettingsInterface $registry;
 
-    private readonly RouteMatcherInterface $matcher;
-
-    private readonly PipelineResolverInterface $resolver;
-
-    public function __construct(RouteMatcherInterface $matcher, PipelineResolverInterface $resolver, BeforePipelineInterface $before, AfterPipelineInterface $after)
+    public function __construct(RouteSettingsInterface $registry, ContainerInterface $container)
     {
-        $this->matcher = $matcher;
-        $this->resolver = $resolver;
-        $this->before = $before;
-        $this->after = $after;
+        $this->registry = $registry;
+        $this->container = $container;
     }
 
     #[NoDiscard]
     #[Override]
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $match = $this->matcher->match($request);
+        [$pipeline, $params] = $this->match($request);
 
-        return (new PipelineRunner($this->resolver->resolve($this->pipeline($request, $match->pipeline))))->handle($request->withAttribute(RouteParams::class, new RouteParams($match->params)));
+        return (new PipelineRunner($this->resolve($pipeline)))->handle($request->withAttribute(RouteParams::class, new RouteParams($params)));
     }
 
     /**
-     * @param iterable<mixed, class-string<MiddlewareInterface>|class-string<RequestHandlerInterface>> $pipeline
-     *
-     * @return iterable<mixed, class-string<MiddlewareInterface>|class-string<RequestHandlerInterface>>
+     * @return array{0: iterable<mixed, mixed>, 1: list<string>}
      */
     #[NoDiscard]
-    private function pipeline(ServerRequestInterface $request, iterable $pipeline): iterable
+    private function match(ServerRequestInterface $request): array
     {
-        yield from $this->before->pipeline($request);
+        $method = $request->getMethod();
+        $path = $request->getUri()->getPath();
 
-        yield from $pipeline;
+        $exact = $this->registry->exact["{$method} {$path}"] ?? $this->registry->exact["* {$path}"] ?? $this->registry->exact["{$method} *"] ?? $this->registry->exact['* *'] ?? null;
 
-        yield from $this->after->pipeline($request);
+        if (is_iterable($exact)) {
+            return [$exact, []];
+        }
+
+        $trie = $this->registry->trie[$method] ?? null;
+
+        if (!is_array($trie)) {
+            return [[], []];
+        }
+
+        $node = $trie;
+        $chunks = explode('/', $path);
+        $params = [];
+
+        foreach ($chunks as $chunk) {
+            if ($chunk === '') {
+                continue;
+            }
+
+            $try = $node[$chunk] ?? null;
+
+            if (is_array($try)) {
+                $node = $try;
+
+                continue;
+            }
+
+            $try = $node['?'] ?? null;
+
+            if (is_array($try)) {
+                $node = $try;
+                $params[] = rawurldecode($chunk);
+
+                continue;
+            }
+
+            return [[], []];
+        }
+
+        $pipeline = $node['#'] ?? null;
+
+        if (is_iterable($pipeline)) {
+            return [$pipeline, $params];
+        }
+
+        return [[], []];
+    }
+
+    /**
+     * @param iterable<mixed, mixed> $pipeline
+     *
+     * @return Iterator<mixed, MiddlewareInterface|RequestHandlerInterface>
+     */
+    #[NoDiscard]
+    private function resolve(iterable $pipeline): Iterator
+    {
+        foreach ($pipeline as $class) {
+            assert(is_string($class));
+
+            $resolved = $this->container->get($class);
+
+            assert($resolved instanceof MiddlewareInterface || $resolved instanceof RequestHandlerInterface);
+
+            yield $resolved;
+        }
     }
 }
